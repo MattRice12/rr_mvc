c7f2068f1e1d22731a22a241556f936c770b9caf
Matt Rice <mattrice12@outlook.com>
2016-06-29 17:52:49 -0500

    404 error shows up when :id is greater than the number of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
-
-    if params[:id]
+    if params[:id].to_i - 1 > User.all.count
+      render "404 PAGE NOT FOUND".to_json, status: "404 NOT FOUND"
+    elsif params[:id]
       render User.all[params[:id].to_i - 1].to_json, status: "200 OK"
     end
   end
--- config/router.rb
+++ config/router.rb
@@ -18,6 +18,7 @@ class Router
     #
     # Put your routes in this array using the get, post, put, delete methods below. (remember order matters)
     [
+
       get('/users/:id', UsersController, :show),
       get('/users', UsersController, :index),
       get('/tweets', TweetsController, :index)
